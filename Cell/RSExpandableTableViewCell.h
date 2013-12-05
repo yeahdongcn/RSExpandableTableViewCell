dbce18fd0c6a5902eb0b8d284727740321e72342
R0CKSTAR <yeahdongcn@gmail.com>
2013-12-05 17:15:07 +0800

    Add public properties for changing ui appearance.

--- Cell/RSExpandableTableViewCell.h
+++ Cell/RSExpandableTableViewCell.h
@@ -10,4 +10,19 @@
 
 @interface RSExpandableTableViewCell : UITableViewCell
 
+/**
+ *  Transform's m34 for content view.
+ */
+@property (nonatomic, assign) CGFloat contentViewTransform_m34; // 0 by default.
+
+/**
+ *  Rotation angle on y axis for content view.
+ */
+@property (nonatomic, assign) CGFloat contentViewRotationAngle_yAxis; // 0 by default.
+
+/**
+ *  EdgeInsets for content view.
+ */
+@property (nonatomic, assign) UIEdgeInsets contentViewEdgeInsets; // UIEdgeInsetsZero by default.
+
 @end
